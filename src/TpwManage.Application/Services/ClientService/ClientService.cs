66c6bcc954677ec2d3346ad612d92d6325325f55
gilbertsilvaa <kemegil515@gmail.com>
2023-12-10 15:59:29 -0300

    :feat: writing and implementing getById, Update and Delete methods in client services.

--- src/TpwManage.Application/Services/ClientService/ClientService.cs
+++ src/TpwManage.Application/Services/ClientService/ClientService.cs
@@ -1,5 +1,6 @@
 using TpwManage.Application.InputModels;
 using TpwManage.Application.ViewModels;
+using TpwManage.Core.Entities;
 using TpwManage.Core.Repositories;
 
 namespace TpwManage.Application.Services.ClientService;
@@ -8,16 +9,44 @@ public class ClientService(IClientRepository repository) : IClientService
 {
   private readonly IClientRepository _repository = repository;
 
+  public async Task<List<ClientViewModel>> GetAll()
+  {
+    try 
+    {
+      var response = await _repository.GetAllAsync();
+      return response.Select(c => ClientViewModel.FromEntity(c)).ToList();
+    }
+    catch(Exception ex)
+    {
+      throw new Exception(ex.Message);
+    }
+  }
+
+  public async Task<ClientViewModel> GetById(Guid id)
+  {
+    try 
+    {
+      var response = await _repository.GetByIdAsync(id) 
+        ?? throw new KeyNotFoundException("Cliente não encontrado.");
+        
+      return ClientViewModel.FromEntity(response);
+    }
+    catch(Exception ex)
+    {
+      throw new Exception(ex.Message);
+    }
+  }
+
   public async Task<ClientViewModel> Create(CreateClientInputModel model)
   {
     try 
     {
-      var client = model.ToEntity();
+      var client = model.ToEntity();     
       var clientExists = await _repository.ExistsAsync(client.Name);
-
+      
       if(clientExists)    
         throw new InvalidOperationException("Já existe um cliente com esse nome.");
-        
+
       var response = await _repository.CreateAsync(client);
       return ClientViewModel.FromEntity(response);
     }
@@ -27,12 +56,27 @@ public class ClientService(IClientRepository repository) : IClientService
     }
   }
 
-  public async Task<List<ClientViewModel>> GetAll()
+  public async Task<ClientViewModel> Update(UpdateClientInputModel model)
   {
     try 
     {
-      var response = await _repository.GetAllAsync();
-      return response.Select(c => ClientViewModel.FromEntity(c)).ToList();
+      var response = await _repository.UpdateAsync(model.ToEntity()) 
+        ?? throw new KeyNotFoundException("Cliente não encontrado.");
+        
+      return ClientViewModel.FromEntity(response);
+    }
+    catch(Exception ex)
+    {
+      throw new Exception(ex.Message);
+    }
+  }
+
+  public async Task<bool> Delete(Guid id)
+  {
+    try 
+    {
+      var response = await _repository.DeleteAsync(id);
+      return response;
     }
     catch(Exception ex)
     {
--- src/TpwManage.Application/Services/ClientService/IClientService.cs
+++ src/TpwManage.Application/Services/ClientService/IClientService.cs
@@ -6,5 +6,8 @@ namespace TpwManage.Application.Services.ClientService;
 public interface IClientService
 {
   Task<List<ClientViewModel>> GetAll();
+  Task<ClientViewModel> GetById(Guid id);
   Task<ClientViewModel> Create(CreateClientInputModel model);
+  Task<ClientViewModel> Update(UpdateClientInputModel model);
+  Task<bool> Delete(Guid id);
 }
