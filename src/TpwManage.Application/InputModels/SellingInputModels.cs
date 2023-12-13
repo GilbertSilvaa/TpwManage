c0834642ec6ba576896fc0fdf712cffc3c571aaa
gilbertsilvaa <kemegil515@gmail.com>
2023-12-12 21:01:46 -0300

    :feat: validating model inputs with DataAnnotations.

--- src/TpwManage.Application/InputModels/SellingInputModels.cs
+++ src/TpwManage.Application/InputModels/SellingInputModels.cs
@@ -1,14 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TpwManage.Application.InputModels;
 
 public class CreateSellingInputModel
 {
+  [Required(ErrorMessage = "ClientId é um campo obrigatório.")]
   public Guid ClientId { get; set; }
+
+  [Required(ErrorMessage = "ProductsId é um campo obrigatório.")]
   public List<Guid> ProductsId { get; set; } = [];
 }
 
 public class UpdateSellingInputModel
 {
+  [Required(ErrorMessage = "Id é um campo obrigatório.")]
   public Guid Id { get; set; }
+
+  [Required(ErrorMessage = "ClientId é um campo obrigatório.")]
   public Guid ClientId { get; set; }
+
+  [Required(ErrorMessage = "ProductsId é um campo obrigatório.")]
   public List<Guid> ProductsId { get; set; } = [];
 }
