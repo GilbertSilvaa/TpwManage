83b561c896cc2edcda4892e1205dec717a492e95
gilbertsilvaa <kemegil515@gmail.com>
2024-02-02 17:31:54 -0300

    :test_tube: test: creation of the Infrastructure layer test base by creating a temporary bench for each test run.

---
+++ test/TpwManage.Infrastructure.Tests/TestBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TpwManage.Infrastructure.Persistence.Context;
+
+namespace TpwManage.Infrastructure.Tests;
+
+public class TestBase { }
+
+public class DbTest : IDisposable
+{
+  private readonly string _dbNameRandom;
+
+  public ServiceProvider ServiceProvider { get; private set; }
+
+  public DbTest()
+  {
+    string dbNameRandom = Guid.NewGuid().ToString().Replace("-", "");
+    _dbNameRandom = $"dbTpwManageTest_{dbNameRandom}";
+
+    string conn = $"Server=localhost;Database={_dbNameRandom};Uid=root;Pwd=senha123;";
+    ServiceCollection serviceCollection = new();
+
+    serviceCollection.AddDbContext<MyContext>(o =>
+      o.UseMySql(conn, ServerVersion.AutoDetect(conn)),
+      ServiceLifetime.Transient);
+    
+    ServiceProvider = serviceCollection.BuildServiceProvider();
+    using var context = ServiceProvider.GetService<MyContext>();
+    context?.Database.EnsureCreated();
+  }
+
+  public void Dispose()
+  {
+    using var context = ServiceProvider.GetService<MyContext>();
+    context?.Database.EnsureDeleted();
+  }
+}
