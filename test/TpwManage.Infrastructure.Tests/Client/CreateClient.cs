229bfb5cd4f7c154c38c37027cc7095c103d64c0
gilbertsilvaa <kemegil515@gmail.com>
2024-02-02 17:32:28 -0300

    :test_tube: test: creation of unit test for the client insertion method.

---
+++ test/TpwManage.Infrastructure.Tests/Client/CreateClient.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TpwManage.Core.Entities;
+using TpwManage.Infrastructure.Persistence.Context;
+using TpwManage.Infrastructure.Persistence.Repositories;
+
+namespace TpwManage.Infrastructure.Tests;
+
+public class CreateClient(DbTest dbTest) : TestBase, IClassFixture<DbTest>
+{
+  private readonly ServiceProvider _serviceProvider = dbTest.ServiceProvider;
+
+  [Fact]
+  public async Task IsPossibleToCreateClient()
+  {
+    using var context = _serviceProvider.GetService<MyContext>();
+    ClientRepository repository = new(context!);
+
+    Client client = new(Faker.Name.FullName());
+    Client clientCreated = await repository.CreateAsync(client);
+
+    Assert.NotNull(clientCreated);
+    Assert.Equal(client.Name, clientCreated.Name);
+    Assert.False(clientCreated.Id == Guid.Empty);
+  }
+}
