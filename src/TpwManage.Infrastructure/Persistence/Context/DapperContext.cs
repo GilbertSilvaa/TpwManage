95817e5a9e6606115dba93bd30500c447c12d89f
gilbertsilvaa <kemegil515@gmail.com>
2024-02-21 20:37:33 -0300

    :sparkles: feat: configure connection dapper.

---
+++ src/TpwManage.Infrastructure/Persistence/Context/DapperContext.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace TpwManage.Infrastructure.Persistence.Context;
+
+public class DapperContext
+{
+  private static MySqlConnection GetConnection()
+    => new(Environment.GetEnvironmentVariable("CONNECTION"));
+
+  public static async Task<List<T>> ExecuteQueryAsync<T>(string SQL)
+  {
+    using var connection = GetConnection();
+    connection.Open();
+
+    return (await connection.QueryAsync<T>(SQL)).ToList();
+  }
+}
